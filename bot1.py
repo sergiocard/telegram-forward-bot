e5285e6e114df71ff978cfeb30326e312c48526d
sergio.cardellicchio <sergio.cardellicchio@nexse.com>
2018-04-21 00:17:11 +0200

    simple

--- bot1.py
+++ bot1.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import config
-import telebot
+import time
+import telepot
+from telepot.loop import MessageLoop
 
-bot = telebot.TeleBot(config.token)
+"""
+After **inserting token** in the source code, run it:
+```
+```
+[Here is a tutorial](http://www.instructables.com/id/Set-up-Telegram-Bot-on-Raspberry-Pi/)
+teaching you how to setup a bot on Raspberry Pi. This simple bot does nothing
+but accepts two commands:
+- `/roll` - reply with a random integer between 1 and 6, like rolling a dice.
+- `/time` - reply with the current time, like a clock.
+"""
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    print 'Got command: %s' % command
+
+bot = telepot.Bot('525287946:AAF0eR_Gtu8f0TEM1SQZ2qyG4qixI8NqDhE')
+
+MessageLoop(bot, handle).run_as_thread()
+print 'I am listening ...'
+
+while 1:
+    time.sleep(10)
